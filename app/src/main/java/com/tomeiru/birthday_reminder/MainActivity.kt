c95fe51680794fa088b0e51a9aea9d3332d3db8e
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-01-11 21:20:48 +0900

    init: initialize repository with Compose Empty Activity

---
+++ app/src/main/java/com/tomeiru/birthday_reminder/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.tomeiru.birthday_reminder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tomeiru.birthday_reminder.ui.theme.BirthdayReminderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayReminderTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BirthdayReminderTheme {
+        Greeting("Android")
+    }
+}
